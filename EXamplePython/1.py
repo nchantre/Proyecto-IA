714696c6d808319e6377b6a85d6d7e0eed68f621
nchantre <nelsonfabiancs@hotmail.com>
2026-02-26 19:57:18 -0500

    Primeros Ejercicios Python

---
+++ EXamplePython/1.py
@@ -0,0 +1,19 @@
+"""
+Programa Simple de Saludo
+Este módulo contiene una función que muestra una cadena de texto por pantalla.
+"""
+
+def mostrar_saludo():
+    """
+    Función que muestra un saludo en pantalla.
+    
+    Returns:
+        None
+    """
+    mensaje = "¡Hola! Bienvenido al programa en Python"
+    print(mensaje)
+
+
+# Punto de entrada del programa
+if __name__ == "__main__":
+    mostrar_saludo()
